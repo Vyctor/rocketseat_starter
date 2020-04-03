816b0440370ea1207fc20a19d7c792023da9cde1
Vyctor <vyctorguimaraes@gmail.com>
2020-04-03 19:05:00 -0300

    Finalizando curso ReactJS

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,12 +4,13 @@ const requireDir = require("require-dir");
 const cors = require("cors");
 
 // Iniciando o APP
-const app = express();
-// permitir enviar dados para a aplicação no formato JSON
-app.use(express.json());
-app.user(cors());
 
-// Inicioando o DB
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Iniciando o DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -19,4 +20,4 @@ requireDir("./src/models");
 // Rotas
 app.use("/api", require("./src/routes"));
 
-app.listen(3001);
+app.listen(4200);
---
+++ reacjs/huntweb/src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.css";
+
+// stateless components => criar componentes diretamente por funções
+const Header = () => <header id="main-header">JSHunt</header>;
+
+export default Header;
